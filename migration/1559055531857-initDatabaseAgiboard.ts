3c7b3e94c960bc32f667458e55be82eadd26d2bf
Kevin Limacher <kevin.limacher@gmail.com>
2019-06-02 17:32:47 +0200

    added migration file

--- migration/1559055531857-initDatabaseAgiboard.ts
+++ migration/1559055531857-initDatabaseAgiboard.ts
@@ -5,56 +5,31 @@ export class initDatabaseAgiboard1559055531857 implements MigrationInterface {
 
     //Perform migration
     public async up(queryRunner: QueryRunner): Promise<any> {
-        //reset database agiboard
-        //await queryRunner.dropTable("user", true);
-        await queryRunner.dropDatabase("agiboard", true);
-        await queryRunner.createDatabase("agiboard", true);
-        await queryRunner.query("use agiboard");
-
-        //creating the migration table
-        await queryRunner.query("CREATE TABLE `agiboard`.`migrations` (`id` int NOT NULL AUTO_INCREMENT, `timestamp` bigint NOT NULL, `name` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
-
-    //-----Creating Tables---
-        //create USER table
-        await queryRunner.query("CREATE TABLE user ("+
-        "userId INT PRIMARY KEY AUTO_INCREMENT,"+
-        "email VARCHAR(255) NOT NULL,"+
-        "password VARCHAR(255) NOT NULL,"+
-        "userStatus ENUM ('Collaborator','External', 'Admin') NOT NULL);");
-
-        //create WORKSPACE table
-        await queryRunner.query("CREATE TABLE workspace ("+
-        "workspaceId INT PRIMARY KEY AUTO_INCREMENT,"+
-        "name VARCHAR(255) NOT NULL,"+
-        "date_creation DATE NOT NULL,"+
-        "userId INT NOT NULL,"+
-        "FOREIGN KEY (userId) REFERENCES user(userId));");
-
-        //create PROJECT table
-        await queryRunner.query("CREATE TABLE project ("+
-        "projectId INT PRIMARY KEY AUTO_INCREMENT,"+
-        "name VARCHAR(255) NOT NULL,"+
-        "description VARCHAR(255) NOT NULL,"+
-        "projectStatus ENUM ('Draft','InProgress', 'Done') NOT NULL,"+
-        "workspaceId INT NOT NULL,"+
-        "FOREIGN KEY (workspaceId) REFERENCES workspace(workspaceId));");
-    
-
     //-----Filling Tables---
         //Add users
-        await queryRunner.query("INSERT INTO user(email, password, userStatus)"+
-        "VALUES ('userCollaborator@gmail.com', 'mdp', 'Collaborator');");
+        await queryRunner.query("INSERT INTO user"+
+        "(name, email, password, creationDate, lastUpdate, admin)"+
+        "VALUES ('user1', 'user1@gmail.com', 'aaaaaaaaaa', NOW(), NOW(), TRUE);");
 
-        await queryRunner.query("INSERT INTO user(email, password, userStatus)"+
-        "VALUES ('userAdmin@gmail.com', 'mdp', 'Admin');");
 
-        await queryRunner.query("INSERT INTO user(email, password, userStatus)"+
-        "VALUES ('userExternal@gmail.com', 'mdp', 'External');");
+        await queryRunner.query("INSERT INTO user"+
+        "(name, email, password, creationDate,lastUpdate, admin)"+
+        "VALUES ('user2', 'user2@gmail.com', 'bbbbbbbbbb', NOW(), NOW(), TRUE);");
+
+        await queryRunner.query("INSERT INTO user"+
+        "(name, email, password, creationDate, lastUpdate, admin)"+
+        "VALUES ('userAdmin', 'userAdmin@gmail.com', 'abc', NOW(), NOW(), TRUE);");
     }
 
     //Reverse migration
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.dropDatabase("agiboard", true);
-    }
+        await queryRunner.query("DELETE FROM user "+
+        "WHERE email='user1@gmail.com';");
 
+        await queryRunner.query("DELETE FROM user "+
+        "WHERE email='user2@gmail.com';");
+
+        await queryRunner.query("DELETE FROM user "+
+        "WHERE email='userAdmin@gmail.com';");
+    }
 }
